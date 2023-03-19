8a12513aab5a21014a3ece1d35a22f79f6581b7b
Alexandr <business-is-real@outlook.com>
2023-03-19 06:15:02 +0300

    complete first task

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
 //и возводит число A в натуральную степень B.
 
+void UserInput(int[]numbers)
+{
+    Console.Write("введите целое число: ");
+    numbers[0] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень числа");
+    numbers[1] = Convert.ToInt32(Console.ReadLine());
+}
 
+int MathPowInt(int [] numbers)
+{
+    int multiplier = numbers[0];
+    for (int i = 1; i < numbers[1]; i++)
+    {
+        numbers[0] = numbers[0] * multiplier;
+    }
+    return numbers[0];
+}
+int[] numbers = new int[2];
+UserInput(numbers);
+System.Console.WriteLine($"Ответ: {MathPowInt(numbers)}");
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
